36129699107b406c9f52a4d8db421a1efe084abf
Michael Gordon <michael.ian.gordon@gmail.com>
2013-03-08 17:34:14 -0500

    adding the jars from the android src distribution for android 15 that include the source of every method in the stubified android.jar that we have been using

---
+++ android-api-src-jars/CheckSource.java
@@ -0,0 +1,46 @@
+import java.util.jar.*;
+import java.util.HashSet;
+import java.util.Enumeration;
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class CheckSource {
+    private static String STUB_FILE = "../android.jar";
+    
+    public static void main(String[] args) throws Exception {
+        JarFile stubJar = new JarFile(STUB_FILE, true);
+        HashSet<String> stubClasses = new HashSet<String>();
+
+        for (Enumeration e = stubJar.entries(); e.hasMoreElements() ;) {
+            JarEntry entry = (JarEntry)e.nextElement();
+            
+            stubClasses.add(entry.getName());
+        }
+
+        File currentDir = new File(".");
+        File[] SRC_FILES = currentDir.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.matches(".*\\.jar");
+                } 
+            });
+
+        for (File srcFile : SRC_FILES) {
+            System.out.println("Using jar src file: " + srcFile);
+            JarFile srcJar = new JarFile(srcFile, true);
+
+             for (Enumeration e = srcJar.entries(); e.hasMoreElements() ;) {
+                 JarEntry entry = (JarEntry)e.nextElement();
+                 
+                 stubClasses.remove(entry.getName());
+             }
+        }
+        
+        System.out.println("\nMissing Classes:");
+        for (String missing : stubClasses) {
+            if (missing.endsWith(".class"))
+                System.out.println(missing);
+        }
+    }
+
+}
