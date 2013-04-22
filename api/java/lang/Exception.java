d0522403f9af659545e7b461230b2a9556f2a910
Steven Graf (GITI) <sgraf@globalinfotek.com>
2013-04-22 14:14:21 -0400

    Added a minimalist model for Throwable and Exception.

---
+++ api/java/lang/Exception.java
@@ -0,0 +1,10 @@
+package java.lang;
+
+import droidsafe.annotations.*;
+import droidsafe.helpers.*;
+
+public class Exception extends Throwable {
+	@DSModeled(DSC.SAFE)
+	public Exception() {
+    }
+}
---
+++ api/java/lang/Throwable.java
@@ -0,0 +1,37 @@
+package java.lang;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import droidsafe.annotations.*;
+import droidsafe.helpers.*;
+import libcore.util.EmptyArray;
+
+public class Throwable implements java.io.Serializable {
+	private DSTaintObject dsTaint = new DSTaintObject();
+	
+	public String getMessage() {
+        return dsTaint.getTaintString();
+    }
+	
+	@DSModeled(DSC.SAFE)
+	public Throwable() {
+		//Class into native methods to fill in the stack state (see below)s
+        //fillInStackTrace();
+    }
+	
+	/*
+	public Throwable fillInStackTrace() {
+        // Fill in the intermediate representation
+        stackState = nativeFillInStackTrace();
+        // Mark the full representation as empty
+        stackTrace = null;
+        return this;
+    }
+    */
+}
