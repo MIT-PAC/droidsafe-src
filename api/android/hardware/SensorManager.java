5f085b4b8457ae678a385c053ee72a50b49830e5
Steven Graf (GITI) <sgraf@globalinfotek.com>
2013-04-24 10:20:41 -0400

    Added additional detail to the SensorManager model.

--- api/android/hardware/SensorManager.java
+++ api/android/hardware/SensorManager.java
@@ -23,7 +23,22 @@ import droidsafe.annotations.*;
 import droidsafe.helpers.*;
 
 public class SensorManager {
+	private static final int SENSOR_DISABLE = -1;
+	private static int sQueue;
+	private static boolean sSensorModuleInitialized = false;
+	private static IWindowManager sWindowManager;
+	private static int sRotation = Surface.ROTATION_0;
+	private static ArrayList<Sensor> sFullSensorsList = new ArrayList<Sensor>();
+	private static SensorEventPool sPool;
+	private static SensorThread sSensorThread;
+	
+	static SparseArray<Sensor> sHandleToSensor = new SparseArray<Sensor>();
+	static final ArrayList<ListenerDelegate> sListeners =
+			new ArrayList<ListenerDelegate>();
+	
 	private DSTaintObject dsTaint = new DSTaintObject();
+	
+	Looper mMainLooper;
 	
 	@DSModeled(value = DSC.SAFE)
 	public SensorManager(Looper mainLooper) {
@@ -33,7 +48,7 @@ public class SensorManager {
 		 * up some internal state managed in private variables.  This   
 		 * functionality will be commented out for now as it isn't necessary
 		 */
-		/*
+		
         mMainLooper = mainLooper;
 
 
@@ -81,6 +96,129 @@ public class SensorManager {
                 sSensorThread = new SensorThread();
             }
         }
-        */
     }
+	
+	public void onRotationChanged(int rotation) {
+        synchronized(sListeners) {
+        	dsTaint.addTaint(rotation);
+            sRotation  = rotation;
+        }
+    }
+	
+	private int getLegacySensorType(int type) {
+		/*
+        switch (type) {
+            case Sensor.TYPE_ACCELEROMETER:
+                return SENSOR_ACCELEROMETER;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                return SENSOR_MAGNETIC_FIELD;
+            case Sensor.TYPE_ORIENTATION:
+                return SENSOR_ORIENTATION_RAW;
+            case Sensor.TYPE_TEMPERATURE:
+                return SENSOR_TEMPERATURE;
+        }
+        */
+        return 0;
+    }
+	
+	private static int sensors_module_get_next_sensor(Sensor sensor, int next) { //Formerly a native method
+		return 0;
+	}
+	
+	private static int sensors_module_init() {  //Formerly a native method
+		return 0;
+	}
+	
+	private static void nativeClassInit() {  //Formerly a native method
+		//Noop
+	}
+	
+	@DSModeled(DSC.SAFE)
+	public void unregisterListener(SensorEventListener listener) {
+		unregisterListener((Object)listener);
+    }
+	
+	@DSModeled(DSC.SAFE)
+	private void unregisterListener(Object listener) {
+        if (listener == null) {
+            return;
+        }
+
+        synchronized (sListeners) {
+            final int size = sListeners.size();
+            for (int i=0 ; i<size ; i++) {
+                ListenerDelegate l = sListeners.get(i);
+                if (l.getListener() == listener) {
+                    sListeners.remove(i);
+                    // disable all sensors for this listener
+                    for (Sensor sensor : l.getSensors()) {
+                        disableSensorLocked(sensor);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+	
+	@DSModeled(DSC.SAFE)
+	private boolean disableSensorLocked(Sensor sensor) {
+        for (ListenerDelegate i : sListeners) {
+            if (i.hasSensor(sensor)) {
+                // not an error, it's just that this sensor is still in use
+                return true;
+            }
+        }
+        String name = sensor.getName();
+        int handle = sensor.getHandle();
+        return sensors_enable_sensor(sQueue, name, handle, SENSOR_DISABLE);
+    }
+	
+	@DSModeled(DSC.SAFE)
+	static boolean sensors_enable_sensor(int queue, String name, int sensor, int enable) {
+		//Formerly a native method.  Generated a very simple stub.
+		return false;
+	}
+	
+	private class ListenerDelegate {
+		private final SensorEventListener mSensorEventListener;
+		private final ArrayList<Sensor> mSensorList = new ArrayList<Sensor>();
+		public SparseBooleanArray mSensors = new SparseBooleanArray();
+		
+		private ListenerDelegate() {
+			mSensorEventListener = null;
+		}
+		
+		Object getListener() {
+            return mSensorEventListener;
+        }
+		
+		List<Sensor> getSensors() {
+            return mSensorList;
+        }
+		
+		boolean hasSensor(Sensor sensor) {
+            return mSensors.get(sensor.getHandle());
+        }
+	}
+	
+	private class SensorEventPool {
+		private final int mPoolSize;
+        private final SensorEvent mPool[];
+        private int mNumItemsInPool;
+        
+        private DSTaintObject dsTaint = new DSTaintObject();
+        
+		SensorEventPool(int poolSize) {
+			dsTaint.addTaint(poolSize);
+            mPoolSize = poolSize;
+            mNumItemsInPool = poolSize;
+            mPool = new SensorEvent[poolSize];
+        }
+	}
+	
+	@DSModeled(DSC.SAFE)
+	static private class SensorThread {
+		SensorThread() {
+        }
+	}
 }
