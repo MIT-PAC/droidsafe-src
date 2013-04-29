be28e45908b5cdfd0e4a2166953a7d4d5c4844fa
Nguyen Nguyen (GITI) <nguyen@uwinsoftware.com>
2013-04-29 09:23:56 -0400

    Moved buildUiObjects outside of Resources() constructor as soot stuff is not used by other standalone make targets

--- main/java/droidsafe/android/app/resources/Resources.java
+++ main/java/droidsafe/android/app/resources/Resources.java
@@ -131,6 +131,7 @@ public class Resources {
   public static void resolveManifest(String rootDir)  {
 		try {
 			v = new Resources (new File (rootDir));
+			v.buildSootObjects();
 
 			// Dump manifest information
 			AndroidManifest am = v.manifest;
@@ -224,11 +225,16 @@ public class Resources {
 
 		// Add This point, all resrouces will have been parsed and loaded
 		ResourcesSoot.v().setNumberToStringMap(resource_info);
-
+	}
+	
+	/**
+	 * buildSootObjects():
+	 * 		This method builds internal UI Objects for use in soot analysis
+	 */
+	public void buildSootObjects() {
 		for (Layout layout: layouts) {
 			layout.buildUIObjects(stringNameToRString);
 		}
-
 	}
 
   /**
