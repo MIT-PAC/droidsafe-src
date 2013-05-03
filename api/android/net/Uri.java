cb1d6e1b50e9dfc0a0d7094d006b3c656d092124
Nguyen Nguyen (GITI) <nguyen@uwinsoftware.com>
2013-05-03 13:28:43 -0400

    Flattening adnroid.net.uri

--- api/android/net/Uri.java
+++ api/android/net/Uri.java
@@ -20,7 +20,8 @@ import java.util.Set;
 import droidsafe.annotations.*;
 import droidsafe.helpers.*;
 
-public abstract class Uri implements Parcelable, Comparable<Uri> {
+//public abstract class Uri implements Parcelable, Comparable<Uri> {
+public class Uri {
 	/*
 	private final static int NOT_FOUND = -1;
 	private static final String NOT_CACHED = new String("NOT CACHED");
@@ -30,6 +31,9 @@ public abstract class Uri implements Parcelable, Comparable<Uri> {
 	private static final String DEFAULT_ENCODING = "UTF-8";
 	*/
 	
+	private DSTaintObject dsTaint = new DSTaintObject();
+	private String uriString;
+	
 	@DSModeled(DSC.SAFE)
 	public int hashCode() {
         return toString().hashCode();
@@ -38,29 +42,32 @@ public abstract class Uri implements Parcelable, Comparable<Uri> {
 	@DSModeled(DSC.SAFE)
 	private Uri() {}  //Prevents external subclassing.
 	
-	public boolean equals(Object o) {
-		return true;
-		/*
-		if (!(o instanceof Uri)) {
-            return false;
-        }
-
-        Uri other = (Uri) o;
-
-        return toString().equals(other.toString());
-        */
+	@DSModeled(DSC.SAFE)
+	private Uri(String uri) {
+		this();
+		uriString = uri;
+		dsTaint.addTaint(uri);
 	}
 	
+	@DSModeled(DSC.SAFE)
+	public boolean equals(Object o) {
+		return true;
+	}
+	
+	@DSModeled(DSC.SAFE)
 	public boolean isOpaque() {
         return !isHierarchical();
     }
+	
+	@DSModeled(DSC.SAFE)
 	public boolean isAbsolute() {
         return !isRelative();
     }
 	
+	 
 	@DSModeled(DSC.SAFE)
     public static Uri parse(String uriString) {
-        return new StringUri(uriString);
+        return new Uri(uriString);
     }
 	
 	/* Child classes */
@@ -75,6 +82,7 @@ public abstract class Uri implements Parcelable, Comparable<Uri> {
 	}
 	*/
 	
+	/*
 	private abstract static class AbstractHierarchicalUri extends Uri {
 		@DSModeled(DSC.SAFE)
 		public AbstractHierarchicalUri() {
@@ -82,167 +90,21 @@ public abstract class Uri implements Parcelable, Comparable<Uri> {
 		}
 	}
 	
-	public static final class Builder {
-		
-	}
+	*/
 	
-	private static class StringUri extends AbstractHierarchicalUri {
-		static final int TYPE_ID = 1;
-		
-		private final String uriString;
-		private DSTaintObject dsTaint = new DSTaintObject();
-		
+	public class Builder {
+		public Builder() {}
+	}
+	/*
+	
+	//private static class StringUri extends AbstractHierarchicalUri {
+	private class StringUri extends Uri{
 		@DSModeled(DSC.BAN)  //From original MIT markup
         private StringUri(String uriString) {
-            this.uriString = uriString;
-            dsTaint.addTaint(uriString);
+			super(uriString);
         }
-		
-		@Override
-		public int describeContents() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public void writeToParcel(Parcel dest, int flags) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public int compareTo(Uri arg0) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public String getScheme() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public boolean isHierarchical() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean isRelative() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public String getSchemeSpecificPart() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public String getEncodedSchemeSpecificPart() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public String getAuthority() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public String getEncodedAuthority() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public String getUserInfo() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public String getEncodedUserInfo() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public String getHost() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public int getPort() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public String getPath() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public String getEncodedPath() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public String getQuery() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public String getEncodedQuery() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public String getFragment() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public String getEncodedFragment() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public List<String> getPathSegments() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public String getLastPathSegment() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@DSModeled(DSC.SAFE)
-		@Override
-		public String toString() {
-			return dsTaint.getTaintString();
-			//return uriString;
-		}
-
-		@Override
-		public Builder buildUpon() {
-			// TODO Auto-generated method stub
-			return null;
-		}
 	}
+	*/
 	
 	/*
 	static abstract class AbstractPart {
@@ -421,6 +283,7 @@ public abstract class Uri implements Parcelable, Comparable<Uri> {
 	*/
 	
 	/* Abstract Methods */
+	/*
 	public abstract String getScheme();
 	public abstract boolean isHierarchical();
 	public abstract boolean isRelative();
@@ -442,4 +305,148 @@ public abstract class Uri implements Parcelable, Comparable<Uri> {
 	public abstract String getLastPathSegment();
 	public abstract String toString();
 	public abstract Builder buildUpon();
+	*/
+	@DSModeled()
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@DSModeled()
+	public void writeToParcel(Parcel dest, int flags) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@DSModeled(DSC.SAFE)
+	public int compareTo(Uri arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String getScheme() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public boolean isHierarchical() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public boolean isRelative() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String getSchemeSpecificPart() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String getEncodedSchemeSpecificPart() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String getAuthority() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String getEncodedAuthority() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String getUserInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String getEncodedUserInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String getHost() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public int getPort() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String getPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String getEncodedPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String getQuery() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String getEncodedQuery() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String getFragment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String getEncodedFragment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public List<String> getPathSegments() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String getLastPathSegment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@DSModeled(DSC.SAFE)
+	public String toString() {
+		return dsTaint.getTaintString();
+		//return uriString;
+	}
+
+	@DSModeled()
+	public Builder buildUpon() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
