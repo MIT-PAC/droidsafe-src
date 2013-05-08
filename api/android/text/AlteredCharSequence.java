792157b46528f342a7472c0f0d3890acd322dbc5
Bob Jacobs (GITI) <bob.jacobs.jr@gmail.com>
2013-05-08 10:26:31 -0400

    Added new android.text.AlteredCharSequence.java.

---
+++ api/android/text/AlteredCharSequence.java
@@ -0,0 +1,37 @@
+package android.text;
+
+import droidsafe.annotations.DSC;
+import droidsafe.annotations.DSModeled;
+
+public class AlteredCharSequence implements CharSequence, GetChars {
+	
+	@DSModeled(DSC.SAFE)
+	public String toString() {
+		return new String();
+	}
+
+	@Override
+	public void getChars(int start, int end, char[] dest, int destoff) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public char charAt(int index) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public CharSequence subSequence(int start, int end) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
