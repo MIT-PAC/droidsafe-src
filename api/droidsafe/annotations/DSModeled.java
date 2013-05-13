b3c6b58d23e8299235207e19ccc20b7c2e927e48
Marcel Becker <marcelbecker@gmail.com>
2013-05-13 13:45:31 -0700

    Removed top level files moved to container folder

--- api/droidsafe/annotations/DSModeled.java
+++
@@ -1,9 +0,0 @@
-package droidsafe.annotations;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
-public @interface DSModeled {
-	DSC value() default DSC.SPEC;
-}
