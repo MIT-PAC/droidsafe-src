3f2ac208becf5885c3c3529ca91a8a3b737e991b
Michael Gordon <michael.ian.gordon@gmail.com>
2013-05-16 18:31:44 -0400

    remove some of the harsh checks on javadocs, fix errors in pre-commit script, and some changes to Main

--- main/java/droidsafe/main/Main.java
+++ main/java/droidsafe/main/Main.java
@@ -40,178 +40,182 @@ import droidsafe.utils.SootUtils;
  * 
  */
 public class Main {
-  private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    /** logger field */
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-  // public static final String LOG_TXT_FILE = "droidsafe.log.txt";
-  // public static final String LOG_XML_FILE = "droidsafe.log.xml";
+    /**
+     * Entry point of DroidSafe Tool.
+     * 
+     */
+    public static void main(String[] args) {
+        logger.info("Starting DroidSafe Run");
+        // grab command line args and set some globals
+        Config.v().init(args);
+        // initial project directories and lib jar files
+        Project.v().init();
+        // configure soot and soot classpath
+        SootConfig.init();
+        // load the api classes and modeling classes
+        API.v().init();
+        // load the application classes
+        Project.v().loadClasses();
+        // create the permissions map
+        Permissions.init();
 
+        logger.info("Creating locals for all string constant arguments.");
+        LocalForStringConstantArguments.run();
 
-  /**
-	 */
-  public static void main(String[] args) {
-    logger.info("Starting DroidSafe Run");
-    // grab command line args and set some globals
-    Config.v().init(args);
-    // initial project directories and lib jar files
-    Project.v().init();
-    // configure soot and soot classpath
-    SootConfig.init();
-    // load the api classes and modeling classes
-    API.v().init();
-    // load the application classes
-    Project.v().loadClasses();
-    // create the permissions map
-    Permissions.init();
+        logger.info("Calling scalar optimizations.");
+        ScalarAppOptimizations.run();
 
-    logger.info("Creating locals for all string constant arguments.");
-    LocalForStringConstantArguments.run();
+        logger.info("Create tags for the overriden system methods in user code.");
+        TagImplementedSystemMethods.run();
 
-    logger.info("Calling scalar optimizations.");
-    ScalarAppOptimizations.run();
+        logger.info("Resolving resources and Manifest.");
 
-    logger.info("Create tags for the overriden system methods in user code.");
-    TagImplementedSystemMethods.run();
+        Resources.resolveManifest(Config.v().APP_ROOT_DIR);
 
-    logger.info("Resolving resources and Manifest.");
-
-    Resources.resolveManifest(Config.v().APP_ROOT_DIR);
-
-    logger.info("Resolving String Constants");
-    ResolveStringConstants.run(Config.v().APP_ROOT_DIR);
-
-
-    logger.info("Finding entry points in user code.");
-    EntryPoints.v().calculate();
-
-    logger.info("Creating Harness.");
-    Harness.create();
-
-    logger.info("Setting Harness Main as entry point.");
-    setHarnessMainAsEntryPoint();
-
-    // The JSA analysis fails if it follows AddAllocsForAPICalls.run()
-    if (Config.v().runStringAnalysis) {
-
-      // Predefined hotspots. Should be removed.
-      JSAStrings.v().addArgumentHotspots("<android.content.Intent: void <init>(java.lang.String)>",
-          0);
-      JSAStrings.v().addArgumentHotspots(
-          "<android.content.Intent: android.content.Intent addCategory(java.lang.String)>", 0);
-
-      JSAStrings.v().addArgumentHotspots(
-          "<android.content.Intent: android.content.Intent setAction(java.lang.String)>", 0);
-
-      JSAStrings.v().addArgumentHotspots("<java.net.URI: void <init>(java.lang.String)>", 0);
-      JSAStrings.v().addArgumentHotspots(
-          "<android.content.Intent: android.content.Intent setType(java.lang.String)>", 0);
-
-      JSAStrings
-          .v()
-          .addArgumentHotspots(
-              "<android.widget.Toast: android.widget.Toast makeText(android.content.Context,java.lang.CharSequence,int)>",
-              1);
-
-      JSAStrings
-          .v()
-          .addArgumentHotspots(
-              "<com.example.android.apis.content.PickContact$ResultDisplayer: void <init>(com.example.android.apis.content.PickContact,java.lang.String,java.lang.String)>",
-              1);
-      JSAStrings
-          .v()
-          .addArgumentHotspots(
-              "<com.example.android.apis.content.PickContact$ResultDisplayer: void <init>(com.example.android.apis.content.PickContact,java.lang.String,java.lang.String)>",
-              2);
-
-      JSAStrings.v().addArgumentHotspots(
-          "<android.app.Activity: void setTitle(java.lang.CharSequence)>", 0);
-      JSAStrings.run(Config.v());
+        logger.info("Resolving String Constants");
+        ResolveStringConstants.run(Config.v().APP_ROOT_DIR);
 
 
-      // Debugging.
-      JSAStrings.v().log();
-    }
+        logger.info("Finding entry points in user code.");
+        EntryPoints.v().calculate();
 
-    AddAllocsForAPICalls.run();
+        logger.info("Creating Harness.");
+        Harness.create();
 
-    logger.info("Starting PTA...");
-    GeoPTA.run();
+        logger.info("Setting Harness Main as entry point.");
+        setHarnessMainAsEntryPoint();
 
-    //logger.info("Incorporating XML layout information");
-    // IntegrateXMLLayouts.run();
+        // The JSA analysis fails if it follows AddAllocsForAPICalls.run()
+        if (Config.v().runStringAnalysis) {
 
-    //logger.info("Specializing API Calls");
-    // APICallSpecialization.run();
+            // Predefined hotspots. Should be removed.
+            JSAStrings.v().addArgumentHotspots("<android.content.Intent: void <init>(java.lang.String)>",
+                0);
+            JSAStrings.v().addArgumentHotspots(
+                "<android.content.Intent: android.content.Intent addCategory(java.lang.String)>", 0);
 
-    // all transforms should be done by here!
-    // logger.info("Restarting PTA...");
-    // GeoPTA.release();
-    // GeoPTA.run();
+            JSAStrings.v().addArgumentHotspots(
+                "<android.content.Intent: android.content.Intent setAction(java.lang.String)>", 0);
 
-    // write jimple txt files for all classes so we can analzye them
-    // all transforms should be done by here.
-    if (Config.v().writeJimpleAppClasses) {
-      writeAllAppClasses();
-    }
+            JSAStrings.v().addArgumentHotspots("<java.net.URI: void <init>(java.lang.String)>", 0);
+            JSAStrings.v().addArgumentHotspots(
+                "<android.content.Intent: android.content.Intent setType(java.lang.String)>", 0);
 
-    RCFG.generate();
-    logger.info("Ending DroidSafe Run");
+            JSAStrings
+            .v()
+            .addArgumentHotspots(
+                "<android.widget.Toast: android.widget.Toast makeText(android.content.Context,java.lang.CharSequence,int)>",
+                1);
 
-    logger.info("Starting Attribute Modeling");
-    AttributeModeling.run();
-    logger.info("Finished Attribute Modeling");
+            JSAStrings
+            .v()
+            .addArgumentHotspots(
+                "<com.example.android.apis.content.PickContact$ResultDisplayer: void <init>(com.example.android.apis.content.PickContact,java.lang.String,java.lang.String)>",
+                1);
+            JSAStrings
+            .v()
+            .addArgumentHotspots(
+                "<com.example.android.apis.content.PickContact$ResultDisplayer: void <init>(com.example.android.apis.content.PickContact,java.lang.String,java.lang.String)>",
+                2);
 
-    // print out what modeling is required for this application
-    RequiredModeling.run();
+            JSAStrings.v().addArgumentHotspots(
+                "<android.app.Activity: void setTitle(java.lang.CharSequence)>", 0);
+            JSAStrings.run(Config.v());
 
 
-    if (Config.v().infoFlow) {
-      logger.info("Starting Information Flow Analysis...");
-      InjectedSourceFlows.run();
-      InterproceduralControlFlowGraph.run();
-      InformationFlowAnalysis.run();
-
-      String infoFlowDotFile = Config.v().infoFlowDotFile;
-      if (infoFlowDotFile != null) {
-        try {
-          String infoFlowDotMethod = Config.v().infoFlowDotMethod;
-          if (infoFlowDotMethod != null) {
-            InformationFlowAnalysis.exportDotGraph(Scene.v().getMethod(infoFlowDotMethod),
-                infoFlowDotFile);
-          } else {
-            InformationFlowAnalysis.exportDotGraph(infoFlowDotFile);
-          }
-        } catch (IOException exp) {
-          logger.error(exp.toString());
+            // Debugging.
+            JSAStrings.v().log();
         }
-      }
-      logger.info("Finished Information Flow Analysis...");
+
+        AddAllocsForAPICalls.run();
+
+        logger.info("Starting PTA...");
+        GeoPTA.run();
+
+        //logger.info("Incorporating XML layout information");
+        // IntegrateXMLLayouts.run();
+
+        //logger.info("Specializing API Calls");
+        // APICallSpecialization.run();
+
+        // all transforms should be done by here!
+        // logger.info("Restarting PTA...");
+        // GeoPTA.release();
+        // GeoPTA.run();
+
+        // write jimple txt files for all classes so we can analzye them
+        // all transforms should be done by here.
+        if (Config.v().writeJimpleAppClasses) {
+            writeAllAppClasses();
+        }
+
+        RCFG.generate();
+        logger.info("Ending DroidSafe Run");
+
+        logger.info("Starting Attribute Modeling");
+        AttributeModeling.run();
+        logger.info("Finished Attribute Modeling");
+
+        // print out what modeling is required for this application
+        RequiredModeling.run();
+
+
+        if (Config.v().infoFlow) {
+            logger.info("Starting Information Flow Analysis...");
+            InjectedSourceFlows.run();
+            InterproceduralControlFlowGraph.run();
+            InformationFlowAnalysis.run();
+
+            String infoFlowDotFile = Config.v().infoFlowDotFile;
+            if (infoFlowDotFile != null) {
+                try {
+                    String infoFlowDotMethod = Config.v().infoFlowDotMethod;
+                    if (infoFlowDotMethod != null) {
+                        InformationFlowAnalysis.exportDotGraph(Scene.v().getMethod(infoFlowDotMethod),
+                            infoFlowDotFile);
+                    } else {
+                        InformationFlowAnalysis.exportDotGraph(infoFlowDotFile);
+                    }
+                } catch (IOException exp) {
+                    logger.error(exp.toString());
+                }
+            }
+            logger.info("Finished Information Flow Analysis...");
+        }
+
+        if (Config.v().target.equals("specdump")) {
+            RCFGToSSL.run();
+
+        } else if (Config.v().target.equals("confcheck")) {
+            logger.error("Not implemented yet!");
+        }
+
+        // System.out.print(RCFG.v().toString());
     }
 
-    if (Config.v().target.equals("specdump")) {
-      RCFGToSSL.run();
-
-    } else if (Config.v().target.equals("confcheck")) {
-      logger.error("Not implemented yet!");
+    /**
+     * Set's harness as entry point for Soot.  Run after EntryPoints.
+     */
+    private static void setHarnessMainAsEntryPoint() {
+        List<SootMethod> entryPoints = new LinkedList<SootMethod>();
+        entryPoints.add(Harness.v().getMain());
+        Scene.v().setEntryPoints(entryPoints);
+        // Scene.v().setMainClass(Harness.v().getHarnessClass());
     }
 
-    // System.out.print(RCFG.v().toString());
-
-  }
-
-  private static void setHarnessMainAsEntryPoint() {
-    List<SootMethod> entryPoints = new LinkedList<SootMethod>();
-    entryPoints.add(Harness.v().getMain());
-    Scene.v().setEntryPoints(entryPoints);
-    // Scene.v().setMainClass(Harness.v().getHarnessClass());
-  }
-
-  private static void writeAllAppClasses() {
-    for (SootClass clz : Scene.v().getClasses()) {
-      if (clz.isApplicationClass() /* && Project.v().isSrcClass(clz.toString()) */) {
-        SootUtils.writeByteCodeAndJimple(
-            Project.v().getOutputDir() + File.separator + clz.toString(), clz);
-      }
+    /**
+     * Dump jimple files for all application classes.
+     */
+    private static void writeAllAppClasses() {
+        for (SootClass clz : Scene.v().getClasses()) {
+            if (clz.isApplicationClass() /* && Project.v().isSrcClass(clz.toString()) */) {
+                SootUtils.writeByteCodeAndJimple(
+                    Project.v().getOutputDir() + File.separator + clz.toString(), clz);
+            }
+        }
     }
-  }
 
 }
