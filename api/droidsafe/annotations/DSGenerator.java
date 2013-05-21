93740e296a6912ce5e3bab4f914c25cda6882c91
Bob Jacobs (GITI) <bob.jacobs.jr@gmail.com>
2013-05-21 15:30:19 -0400

    Added api/droidsafe/annotations/DSGenerator.java and api/droidsafe/annotations/DSOverride.java to support automation.

---
+++ api/droidsafe/annotations/DSGenerator.java
@@ -0,0 +1,13 @@
+package droidsafe.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DSGenerator {
+	String key();
+	String value();
+}
