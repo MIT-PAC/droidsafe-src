175f228f441bb294ed815940cbfd0b2ddf6ba310
Nguyen Nguyen (GITI) <nguyen@uwinsoftware.com>
2013-06-11 13:24:41 -0400

    Added convenient constants for modeling in DSUtils

--- api/droidsafe/helpers/DSUtils.java
+++ api/droidsafe/helpers/DSUtils.java
@@ -12,6 +12,7 @@ public class DSUtils {
 	public final static long 	UNKNOWN_LONG  = 0;
 	public final static int     UNKNOWN_INT   = 0;
 	public final static short   UNKNOWN_SHORT = 0;
+	public final static byte    UNKNOWN_BYTE  = 0;
 	public final static char	UNKNOWN_CHAR  = 0;
 	public final static boolean UNKNOWN_BOOLEAN = false;
 	
