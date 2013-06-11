0a74565492f67972c910738b4eb3606c86cadc39
Nguyen Nguyen (GITI) <nguyen@uwinsoftware.com>
2013-06-11 13:07:06 -0400

    Added convenient constants for modeling in DSUtils

--- api/droidsafe/helpers/DSUtils.java
+++ api/droidsafe/helpers/DSUtils.java
@@ -6,7 +6,14 @@ import droidsafe.annotations.DSC;
 import droidsafe.annotations.DSModeled;
 
 public class DSUtils {
-	public final static String UNKNOWN_STRING = "";
+	public final static String  UNKNOWN_STRING = "";
+	public final static double  UNKNOWN_DOUBLE = 0.0;
+	public final static float	UNKNOWN_FLOAT  = 0.0F;
+	public final static long 	UNKNOWN_LONG  = 0;
+	public final static int     UNKNOWN_INT   = 0;
+	public final static short   UNKNOWN_SHORT = 0;
+	public final static char	UNKNOWN_CHAR  = 0;
+	public final static boolean UNKNOWN_BOOLEAN = false;
 	
 	/**
 	 * Use this method to translate one intent to another in a way that is
