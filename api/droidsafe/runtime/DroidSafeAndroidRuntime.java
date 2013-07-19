be97969d4f8159c5921e431453eb17df8380378b
Nguyen Nguyen (GITI) <nguyen@uwinsoftware.com>
2013-07-19 17:27:52 -0400

    intent filter good now

--- api/droidsafe/runtime/DroidSafeAndroidRuntime.java
+++ api/droidsafe/runtime/DroidSafeAndroidRuntime.java
@@ -5,7 +5,11 @@ import java.util.Random;
 import android.app.ContextImpl;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.app.Application;
+
+import droidsafe.annotations.DSC;
 import droidsafe.annotations.DSModeled;
 
 /**
@@ -31,7 +35,6 @@ public class DroidSafeAndroidRuntime {
 		
 	}
 	
-	@DSModeled
 	/**
 	 * create any associated state and call init methods on an activity
 	 * 
@@ -39,6 +42,7 @@ public class DroidSafeAndroidRuntime {
 	 * 
 	 * @param activity
 	 */
+	@DSModeled(DSC.SPEC)
 	public static void modelActivity(android.app.Activity activity) {
 		ContextImpl context = new ContextImpl();
 		
@@ -56,19 +60,29 @@ public class DroidSafeAndroidRuntime {
 		//code
 	}
 	
-
-	
+	@DSModeled(DSC.SPEC)
 	public static void modelService(android.app.Service service) {
 
 	}
 	
+	@DSModeled(DSC.SPEC)
 	public static void modelContentProvider(android.content.ContentProvider contentProvider) {
 		
 	}
 	
-	@DSModeled
+	@DSModeled(DSC.SPEC)
 	public static void modelBroadCastReceiver(BroadcastReceiver receiver) {
 		receiver.onReceive(new ContextImpl(), new Intent());
+	}
+	
+	@DSModeled(DSC.SPEC)
+	public static void modelApplication(android.app.Application app) {
+		while (true) {
+			app.droidsafeOnCreate();
+			app.droidsafeOnTerminate();
+			app.droidsafeOnEverythingElse();
+		}
+		//code
 	}
 	
 }
