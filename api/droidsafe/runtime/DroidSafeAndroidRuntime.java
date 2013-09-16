ecee8be08ff3afa68cba691add1011c63b97e0db
Michael Gordon <michael.ian.gordon@gmail.com>
2013-09-16 11:48:29 -0400

    removing infinite loops from model, rely on harness for the infinite loop

--- api/droidsafe/runtime/DroidSafeAndroidRuntime.java
+++ api/droidsafe/runtime/DroidSafeAndroidRuntime.java
@@ -28,111 +28,106 @@ import android.app.Application;
  *
  */
 public class DroidSafeAndroidRuntime {
-	public static boolean control = new Random().nextBoolean();
-	public static int switchControl = new Random().nextInt();
-	private static Application mApplication;
-	
-	@DSModeled(DSC.BAN)
-	/**
-	 * This method will be called automatically by the droidsafe harness class before all
-	 * application code.
-	 * 	
-	 * @param args
-	 */
-	public static void main() {
-		
-	}
-	
-	/**
-	 * create any associated state and call init methods on an activity
-	 * 
-	 * call any life cycle events for the activity
-	 * 
-	 * @param activity
-	 */
-	@DSModeled(DSC.BAN)
-	public static void modelActivity(android.app.Activity activity) {
-		ContextImpl context = new ContextImpl();
-		
-		if (mApplication != null)
-			activity.setApplication(mApplication);
-		
-		while (true) {
-			Bundle b = new Bundle();
-			activity.performCreate(b, context);
+    public static boolean control = new Random().nextBoolean();
+    public static int switchControl = new Random().nextInt();
+    private static Application mApplication;
 
-			activity.onAttachedToWindow();
-			activity.onWindowFocusChanged(true);
+    @DSModeled(DSC.BAN)
+    /**
+     * This method will be called automatically by the droidsafe harness class before all
+     * application code.
+     * 	
+     * @param args
+     */
+    public static void main() {
 
-			// Call all public methods with no params
-			activity.onBackPressed();
-			activity.onContentChanged();
-			activity.onCreateDescription();
-			activity.onLowMemory();
-			activity.onRetainNonConfigurationInstance();
-			activity.onSearchRequested();
-			activity.onUserInteraction();
-			
-			activity.onConfigurationChanged(new Configuration());
-			
-			activity.droidsafeOnSavedInstanceState(new Bundle());
-			
-			activity.droidsafeOnResume();
-			activity.droidsafeOnPause();
-			activity.droidsafeOnStop();
-			activity.droidsafeOnDestroy();
-			activity.onDetachedFromWindow();
-		}
-		
-		//code
-	}
-	
-	public static void modelService(android.app.Service service) {
-		if (mApplication != null)
-			service.setApplication(mApplication);
+    }
 
-		service.onCreate();
-		for (IntentFilter filter : service.__ds__intentFilters) {
-			for (Intent intent : DSUtils.getIntentFromFilter(filter)) {
-				service.onBind(intent);
-				service.onRebind(intent);
-				service.onStart(intent, 0);
-				service.onTaskRemoved(intent);
-				service.onStartCommand(intent, 0, 0);
-				service.onUnbind(intent);
-				if (service instanceof IntentService) {
-					((IntentService) service).__ds__onHandleIntent(intent);
-				}
-			}
-		}
-		service.onConfigurationChanged(new Configuration());
-		service.onLowMemory();
-		service.onTrimMemory(0);
-		service.stopSelf(0);
-		service.onDestroy();
-	}
-	
-	@DSModeled(DSC.SPEC)
-	public static void modelContentProvider(android.content.ContentProvider contentProvider) {
-		contentProvider.onCreate();
-		contentProvider.onConfigurationChanged(new Configuration());
-		contentProvider.onLowMemory();
-		contentProvider.onTrimMemory(0);
-	}
-	
-	@DSModeled(DSC.SPEC)
-	public static void modelBroadCastReceiver(BroadcastReceiver receiver) {
-		// No-op onReceive called from registerReceiver in ContextImpl
-	}
-	
-	@DSModeled(DSC.SPEC)
-	public static void modelApplication(android.app.Application app) {
-		mApplication = app;
-		while (true) {
-			app.droidsafeOnCreate();
-			app.droidsafeOnTerminate();
-			app.droidsafeOnEverythingElse();
-		}
-		//code
-	}
+    /**
+     * create any associated state and call init methods on an activity
+     * 
+     * call any life cycle events for the activity
+     * 
+     * @param activity
+     */
+    @DSModeled(DSC.BAN)
+    public static void modelActivity(android.app.Activity activity) {
+        ContextImpl context = new ContextImpl();
+
+        if (mApplication != null)
+            activity.setApplication(mApplication);
+
+        Bundle b = new Bundle();
+        activity.performCreate(b, context);
+
+        activity.onAttachedToWindow();
+        activity.onWindowFocusChanged(true);
+
+        // Call all public methods with no params
+        activity.onBackPressed();
+        activity.onContentChanged();
+        activity.onCreateDescription();
+        activity.onLowMemory();
+        activity.onRetainNonConfigurationInstance();
+        activity.onSearchRequested();
+        activity.onUserInteraction();
+
+        activity.onConfigurationChanged(new Configuration());
+
+        activity.droidsafeOnSavedInstanceState(new Bundle());
+
+        activity.droidsafeOnResume();
+        activity.droidsafeOnPause();
+        activity.droidsafeOnStop();
+        activity.droidsafeOnDestroy();
+        activity.onDetachedFromWindow();
+
+    }
+
+    public static void modelService(android.app.Service service) {
+        if (mApplication != null)
+            service.setApplication(mApplication);
+
+        service.onCreate();
+        for (IntentFilter filter : service.__ds__intentFilters) {
+            for (Intent intent : DSUtils.getIntentFromFilter(filter)) {
+                service.onBind(intent);
+                service.onRebind(intent);
+                service.onStart(intent, 0);
+                service.onTaskRemoved(intent);
+                service.onStartCommand(intent, 0, 0);
+                service.onUnbind(intent);
+                if (service instanceof IntentService) {
+                    ((IntentService) service).__ds__onHandleIntent(intent);
+                }
+            }
+        }
+        service.onConfigurationChanged(new Configuration());
+        service.onLowMemory();
+        service.onTrimMemory(0);
+        service.stopSelf(0);
+        service.onDestroy();
+    }
+
+    @DSModeled(DSC.SPEC)
+    public static void modelContentProvider(android.content.ContentProvider contentProvider) {
+        contentProvider.onCreate();
+        contentProvider.onConfigurationChanged(new Configuration());
+        contentProvider.onLowMemory();
+        contentProvider.onTrimMemory(0);
+    }
+
+    @DSModeled(DSC.SPEC)
+    public static void modelBroadCastReceiver(BroadcastReceiver receiver) {
+        // No-op onReceive called from registerReceiver in ContextImpl
+    }
+
+    @DSModeled(DSC.SPEC)
+    public static void modelApplication(android.app.Application app) {
+        mApplication = app;
+
+        app.droidsafeOnCreate();
+        app.droidsafeOnTerminate();
+        app.droidsafeOnEverythingElse();
+    }
 }
