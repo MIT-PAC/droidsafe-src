07d9c42a932f673ed2bbbdf4d7356b5fa528050b
Michael Gordon <michael.ian.gordon@gmail.com>
2013-10-23 19:09:33 -0400

    explicitly call LocationListener methods in event listener registration method

--- api/android/location/LocationManager.java
+++ api/android/location/LocationManager.java
@@ -253,6 +253,7 @@ String var540C13E9E156B687226421B24F2DF178_11489280 =         null;
     }
 
     
+ 
     @DSModeled(DSC.SAFE)
     @DSGenerator(tool_name = "Doppelganger", tool_version = "0.4.2", generated_on = "2013-07-18 10:21:36.985 -0400", hash_original_method = "79B0FCFC5B63DF5D944678DA62AFE363", hash_generated_method = "52636F7D8FC2E80863E9DF747C3029E6")
     public void requestLocationUpdates(String provider,
@@ -261,6 +262,7 @@ String var540C13E9E156B687226421B24F2DF178_11489280 =         null;
         addTaint(minDistance);
         addTaint(minTime);
         addTaint(provider.getTaint());
+                
         if(provider == null)        
         {
             IllegalArgumentException var3740E938BA9AB726EFDC662393650C35_812000166 = new IllegalArgumentException("provider==null");
@@ -363,6 +365,14 @@ String var540C13E9E156B687226421B24F2DF178_11489280 =         null;
         addTaint(minTime);
         addTaint(criteria.getTaint());
         addTaint(provider.getTaint());
+        
+        //call listeners directory for DroidSafe
+        listener.onLocationChanged(new Location(provider));
+        Bundle bundle = new Bundle();
+        listener.onStatusChanged(provider, 0, bundle);
+        listener.onProviderEnabled(provider);
+        listener.onProviderDisabled(provider);
+        
         if(minTime < 0L)        
         {
             minTime = 0L;
