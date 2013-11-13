93f7d30d70cef9b5b9c84435727c0e2550fe744a
Dmitrij Petters <Dpetters91@gmail.com>
2013-11-13 16:17:19 -0500

    disabled html escaping for pretty printing of VA results

--- main/java/droidsafe/analyses/value/VAModel.java
+++ main/java/droidsafe/analyses/value/VAModel.java
@@ -49,12 +49,12 @@ public abstract class VAModel {
      * @returns a well-formatted (pretty!) detailed printout of the results
      */
     public String toStringPretty() {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
         JsonParser jp = new JsonParser();
         try {
             JsonElement je = jp.parse(this.toStringDetailed());
-            // return gson.toJson(je).replace("\"", ""); 
-            return unescapeUnicode(gson.toJson(je).replace("\"", "")); 
+            return gson.toJson(je).replace("\"", ""); 
+            //return unescapeUnicode(gson.toJson(je).replace("\"", "")); 
         } catch (JsonSyntaxException e) {
             System.out.println("Malformed JSON: " + this.toStringDetailed());
             System.out.println(e.toString());
@@ -67,43 +67,5 @@ public abstract class VAModel {
      */
     public String toString() {
         return this.toStringDetailed().replace("\"", "");
-    }
-
-    public String unescapeUnicode(String input) {
-        StringBuffer buf = new StringBuffer();
-        int index = 0;
-        while (index < input.length()) {
-            if (input.charAt(index) == '\\' && index + 1 < input.length() && input.charAt(index + 1) == 'u') {
-                // consume optional additional 'u' chars
-                int i = 2;
-                while (index + i < input.length() && input.charAt(index + i) == 'u') {
-                    i++;
-                }
-
-                if (index + i < input.length() && input.charAt(index + i) == '+') {
-                    i++;
-                }
-
-                if (index + i + 4 <= input.length()) {
-                    // Get 4 hex digits
-                    CharSequence unicode = input.subSequence(index + i, index + i + 4);
-
-                    try {
-                        int value = Integer.parseInt(unicode.toString(), 16);
-                        buf.append((char) value);
-                    } catch (NumberFormatException nfe) {
-                        throw new IllegalArgumentException("Unable to parse unicode value: " + unicode, nfe);
-                    }
-                    index += i + 4;
-                } else {
-                    throw new IllegalArgumentException("Less than 4 hex digits in unicode value: '" + input.subSequence(index, input.length())
-                        + "' due to end of CharSequence");
-                }
-            } else {
-                buf.append(input.charAt(index));
-                index++;
-            }
-        }
-        return buf.toString();
     }
 }
