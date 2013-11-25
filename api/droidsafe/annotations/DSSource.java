e1e548425498cb6d5ed1e5b119852d618e21fdef
Nguyen Nguyen (GITI) <nguyen@uwinsoftware.com>
2013-11-25 11:33:43 -0500

    Added new annotations DSCat, DSSink, DSSource, DSSinkKind, DSSourceKind

---
+++ api/droidsafe/annotations/DSSource.java
@@ -0,0 +1,13 @@
+package droidsafe.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DSSource{
+    DSSourceKind[] value() default {};
+}
+
