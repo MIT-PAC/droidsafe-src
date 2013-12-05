3f09960dd95f01d36be2aa978a37bb601c425c70
Michael Gordon <michael.ian.gordon@gmail.com>
2013-12-05 17:31:03 -0500

    adding new allocation graph with worklist algorithm

---
+++ main/java/droidsafe/transforms/objsensclone/AllocationGraph.java
@@ -0,0 +1,180 @@
+package droidsafe.transforms.objsensclone;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.jgrapht.ext.DOTExporter;
+import org.jgrapht.ext.VertexNameProvider;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DirectedMultigraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import droidsafe.analyses.pta.PTABridge;
+import droidsafe.android.app.Project;
+import droidsafe.utils.SootUtils;
+import soot.Body;
+import soot.RefType;
+import soot.SootClass;
+import soot.SootMethod;
+import soot.jimple.AssignStmt;
+import soot.jimple.NewExpr;
+import soot.jimple.Stmt;
+import soot.jimple.StmtBody;
+import soot.util.Chain;
+
+public class AllocationGraph {
+    /** static logger class */
+    private static final Logger logger = LoggerFactory.getLogger(AllocationGraph.class);
+    
+    private DirectedMultigraph<SootClass, DefaultEdge> graph;
+        
+    public AllocationGraph() {
+        graph = new DirectedMultigraph<SootClass, DefaultEdge>(DefaultEdge.class);
+        buildAllocationGraph();
+        exportToDot(Project.v().getOutputDir() + File.separator + 
+            "allocation-graph.dot");
+        
+        System.out.println("Allocation graph size: " + graph.vertexSet().size());
+        
+        for (SootClass clz : workList()) {
+            System.out.println(clz);
+        }
+        
+        System.out.println("*************");
+        
+    }
+    
+    private  void buildAllocationGraph() {
+        SootMethod[] methods = PTABridge.v().getAllReachableMethods().toArray(new SootMethod[0]);
+
+        for (SootMethod method : methods) {
+            //we are not cloning static methods, so it does not matter if an allocation is in a static
+            //method, we should not clone because of it, so don't add the effects of static methods
+            //to the allocation graph
+            if (method.isStatic())
+                continue;
+            
+            if (method.isAbstract() || !method.isConcrete())
+                continue;
+
+            SootClass enclosingClass = method.getDeclaringClass();
+
+            Body body = method.getActiveBody();
+            StmtBody stmtBody = (StmtBody)body;
+            Chain units = stmtBody.getUnits();
+            Iterator stmtIt = units.snapshotIterator();
+
+            while (stmtIt.hasNext()) {
+                Stmt stmt = (Stmt)stmtIt.next();
+
+                if (stmt instanceof AssignStmt) {
+                    AssignStmt assign = (AssignStmt) stmt;
+                    if (assign.getRightOp() instanceof NewExpr) {
+                        NewExpr newExpr = (NewExpr)assign.getRightOp();
+                        SootClass newClass = newExpr.getBaseType().getSootClass();
+                        //is exception?
+                        if (!SootUtils.isSubTypeOfIncluding(RefType.v(newClass), RefType.v("java.lang.Throwable"))) {
+                            graph.addVertex(enclosingClass);
+                            graph.addVertex(newClass);
+                            graph.addEdge(enclosingClass, newClass);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public List<SootClass> workList() {
+        List<SootClass> trav = new LinkedList<SootClass>();
+        
+        Set<SootClass> unvisited = new LinkedHashSet<SootClass>();
+        unvisited.addAll(graph.vertexSet());
+        
+        //remove all classes with zero incoming from the graph
+        for (SootClass clz : graph.vertexSet()) {
+            if (graph.inDegreeOf(clz) == 0)
+                unvisited.remove(clz);
+        }
+        
+        while (!unvisited.isEmpty()) {
+            //find vertex with min number of incoming unvisited
+            int min = Integer.MAX_VALUE;
+            SootClass minVertex = null;
+            
+            for (SootClass clz : unvisited) {
+                int incomingUnvisited = 0;
+                for (DefaultEdge inEdge : graph.incomingEdgesOf(clz)) {
+                    if (unvisited.contains(graph.getEdgeSource(inEdge)))
+                        incomingUnvisited++;
+                }
+                
+                //here we have a count of unvisited incoming
+                //short circuit zero case
+                if (incomingUnvisited == 0) {
+                    minVertex = clz;
+                    min = 0;
+                    break;
+                } else if (incomingUnvisited < min) {
+                    min = incomingUnvisited;
+                    minVertex = clz;
+                }
+            }
+            
+            //at this point we have a clz with min number of incoming uncloned
+            unvisited.remove(minVertex);
+            trav.add(minVertex);
+        }
+        
+        return trav;
+    }
+    
+    public List<SootClass> breadthFirst() {
+        List<SootClass> trav = new LinkedList<SootClass>();
+        
+        BreadthFirstIterator<SootClass, DefaultEdge> bf = 
+                new BreadthFirstIterator<SootClass, DefaultEdge>(graph);
+
+        while (bf.hasNext()) {
+            trav.add(bf.next());
+        }
+        
+        System.out.println("BFS traversal size: " + trav.size());
+        
+        return trav;
+    }
+
+    public void exportToDot(String filename) {
+        DOTExporter<SootClass, DefaultEdge> dotE = new DOTExporter<SootClass, DefaultEdge>(
+                new VertexNameProvider<SootClass>() {
+                    @Override
+                    public String getVertexName(SootClass arg0) {
+                        return arg0.hashCode() + "";
+                    }
+                }, 
+                new VertexNameProvider<SootClass>() {
+                    @Override
+                    public String getVertexName(SootClass arg0) {
+                        return arg0.getName();
+                    }
+                },
+                null);
+                
+        try {
+            dotE.export(new FileWriter(filename), graph);
+        } catch (IOException e) {
+            logger.error("Error writing allocation graph dot file!");
+            droidsafe.main.Main.exit(1);
+        }
+    }
+}
