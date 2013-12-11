7d4bcaf25073c8a21c49cb11dbd0bc569c5d4422
Nguyen Nguyen (GITI) <nguyen@uwinsoftware.com>
2013-12-11 16:27:50 -0500

    more catagories

--- api/droidsafe/annotations/DSCat.java
+++ api/droidsafe/annotations/DSCat.java
@@ -1,23 +1,57 @@
 package droidsafe.annotations;
 
 public enum DSCat {
-    ANDROID_INTERNAL,
-    ANDROID_MANAGER,
+    //Safe Categories */
+    ANDROID_ANIMATION,
+    OS_GENERAL,
+    CONTAINER_CLASS,
+    DATA_STRUCTURE,  
+    FRAMEWORK_FINAL,  //cannot override
+    SAFE_LIST,
+    SAFE_OTHERS,
+    
+    //Ban Categories
     DALVIK,
-    DEFAULT_MODIFIER,
-    GPS,
-    GUI,
-    INTERNET,
-    IO,
-    IO_ACTION_METHOD,
-    LOGGING,
-    LOCATION,    
     PRIVATE_METHOD,
     REFLECTION,
-    SAFE_LIST,
+    
+    //Spec Categories
+    ANDROID_APPLICATION,
+    ANDROID_INTERNAL,
+    ANDROID_INSTRUMENTATION,
+    ANDROID_MANAGER,
+    CONTENT,
+    TO_MODEL, //once the method is modeled, it will be safe
+
+    DEFAULT_MODIFIER,  
+    DEVICE,
+
+    GPS,
+    GRAPHICS,
+    GUI,    
+    INTERNET,
+    INTENT_EXCHANGE,
+    IO,
+    IO_ACTION_METHOD,
+    JAVA_SECURITY,
+    LOGGING,
+    LOCATION,    
+    MEDIA_RECORDER,
+    NETWORK_STATS,
+    NETWORKING,
+    
+    OS_LOW_LEVEL,    
+    PHONE_CALL,
     SMS,
+    SHARING,
     STORAGE,
+    SYSTEM_SETTING,
     TELEPHONY,
+    URI_EXCHANGE,
     WIFI,
+    
+    SPEC_OTHERS,
+    
+    //Default value, not SAFE, BAN nor SPEC
     UNSPECIFIED;
 }
