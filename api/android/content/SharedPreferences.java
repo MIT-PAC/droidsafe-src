b8a6e24175d3124999a90de8101541625d2c5e4e
Nguyen Nguyen <nguyen@uwinsoftware.com>
2014-02-10 11:13:08 -0500

    Incremental modeling

--- api/android/content/SharedPreferences.java
+++ api/android/content/SharedPreferences.java
@@ -16,67 +16,89 @@ public interface SharedPreferences {
     
     
     public interface Editor {
-        
+        @DSVerified("no callback, delegated to implementation")
+        @DSSpec(DSCat.ABSTRACT_METHOD)
         Editor putString(String key, String value);
         
-        
+        @DSVerified("no callback, delegated to implementation")
+        @DSSpec(DSCat.ABSTRACT_METHOD)
         Editor putStringSet(String key, Set<String> values);
         
-        
+        @DSVerified("no callback, delegated to implementation")
+        @DSSpec(DSCat.ABSTRACT_METHOD)
         Editor putInt(String key, int value);
         
-        
+        @DSVerified("no callback, delegated to implementation")
+        @DSSpec(DSCat.ABSTRACT_METHOD)
         Editor putLong(String key, long value);
         
-        
+        @DSVerified("no callback, delegated to implementation")
+        @DSSpec(DSCat.ABSTRACT_METHOD)
         Editor putFloat(String key, float value);
         
-        
+        @DSVerified("no callback, delegated to implementation")
+        @DSSpec(DSCat.ABSTRACT_METHOD)
         Editor putBoolean(String key, boolean value);
 
-        
+        @DSVerified("no callback, delegated to implementation")
+        @DSSpec(DSCat.ABSTRACT_METHOD)
         Editor remove(String key);
 
-        
+        @DSVerified("no callback, delegated to implementation")
+        @DSSpec(DSCat.ABSTRACT_METHOD)
         Editor clear();
 
-        
+        @DSVerified("no callback, delegated to implementation")
+        @DSSpec(DSCat.ABSTRACT_METHOD)
         boolean commit();
 
-        
+        @DSVerified("no callback, delegated to implementation")
+        @DSSpec(DSCat.ABSTRACT_METHOD)
         void apply();
     }
 
-    
+    @DSVerified("no callback, delegated to implementation")
+    @DSSpec(DSCat.ABSTRACT_METHOD)
     Map<String, ?> getAll();
 
-    
+    @DSVerified("no callback, delegated to implementation")
+    @DSSpec(DSCat.ABSTRACT_METHOD)
     String getString(String key, String defValue);
     
-    
+    @DSVerified("no callback, delegated to implementation")
+    @DSSpec(DSCat.ABSTRACT_METHOD)
     Set<String> getStringSet(String key, Set<String> defValues);
     
-    
+    @DSVerified("no callback, delegated to implementation")
+    @DSSpec(DSCat.ABSTRACT_METHOD)
     int getInt(String key, int defValue);
     
-    
+    @DSVerified("no callback, delegated to implementation")
+    @DSSpec(DSCat.ABSTRACT_METHOD)
     long getLong(String key, long defValue);
     
-    
+    @DSVerified("no callback, delegated to implementation")
+    @DSSpec(DSCat.ABSTRACT_METHOD)
     float getFloat(String key, float defValue);
     
-    
+    @DSVerified("no callback, delegated to implementation")
+    @DSSpec(DSCat.ABSTRACT_METHOD)
     boolean getBoolean(String key, boolean defValue);
 
-    
+    @DSVerified("no callback, delegated to implementation")
+    @DSSpec(DSCat.ABSTRACT_METHOD)
     boolean contains(String key);
     
-    
+    @DSVerified("no callback, delegated to implementation")
+    @DSSpec(DSCat.ABSTRACT_METHOD)
     Editor edit();
     
-    
+
+    @DSVerified("no callback, delegated to implementation")
+    @DSSpec(DSCat.ABSTRACT_METHOD)
     void registerOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener);
     
-    
+    @DSVerified("no callback, delegated to implementation")
+    @DSSpec(DSCat.ABSTRACT_METHOD)
     void unregisterOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener);
 }
