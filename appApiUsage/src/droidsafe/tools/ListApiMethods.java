1a9edbbd25dcedc88141cf2efa4dff38d4305298
Nguyen Nguyen <nguyen@uwinsoftware.com>
2014-02-19 11:22:15 -0500

    improved tool to matchup engagement 3 APIs and current models

--- appApiUsage/src/droidsafe/tools/ListApiMethods.java
+++ appApiUsage/src/droidsafe/tools/ListApiMethods.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -24,22 +26,125 @@ import org.slf4j.LoggerFactory;
 import soot.Scene;
 import soot.SootClass;
 import soot.SootMethod;
+import soot.tagkit.*;
 
 public class ListApiMethods extends ApiUsageListing {
     private static final Logger logger = LoggerFactory.getLogger(ApiUsageListing.class);
 
     private Set<String> safeClassSet = new HashSet<String>();
     
+    private Set<String> interestList = new HashSet<String>();
     public ListApiMethods() {
         super();
     }
     
+    /**
+     * set the read list, will try to read the file
+     * The file can be in the format:  class method  | class.method (no signature)
+     * @param readList
+     */
+    public void setReadList(String readList) {
+        InputStream listFile;
+        try {
+            logger.warn("adding interest list");
+            listFile = new FileInputStream(readList);
+            List<String> list = IOUtils.readLines(listFile);
+            
+            for (String entry: list) {
+                entry = entry.trim();
+                entry = entry.replaceAll("\\d+\\s+",  "");
+                entry = entry.replaceAll("\\s+",  ".");
+                logger.debug("adding {}", entry);
+                interestList.add(entry);
+            }
+        }
+        catch (Exception ex) {
+            return;
+        }
+    }
+    
+
+    String getMethodFullName(SootMethod method) {
+        String sig = method.getDeclaringClass().toString() + "." + method.getName();
+        return sig;
+    }
+    private boolean matchedMethodSignature(String sig) {        
+        if (interestList.size() == 0)
+            return true;
+        return interestList.contains(sig);
+    }
+    
     public void saveApiList(String fileName, boolean useModifier, boolean classify) throws FileNotFoundException {
         
+        int i = 0;
         PrintStream outStream = new PrintStream(fileName);
+        Set<String> matchedSet = new HashSet<String>();
         for(SootClass cls: Scene.v().getClasses()) {
             for (SootMethod method: cls.getMethods()) {
-                String line = '"' + method.getSignature() + '"';
+               
+                String classificationAnno = null;
+                String commentAnno = null;
+                //Extracting classification annotation
+                for (Tag tag: method.getTags()) {
+                    logger.debug("{} => {}/{}", method, tag.getClass(), tag);
+                    if (!(tag instanceof soot.tagkit.VisibilityAnnotationTag)) {
+                        continue;
+                    }
+                    VisibilityAnnotationTag visAnnotation = (VisibilityAnnotationTag)tag;
+
+                    logger.debug("visAnnotation {} ", visAnnotation);
+
+                    StringBuilder sb = new StringBuilder();
+                    for (AnnotationTag annoTag: visAnnotation.getAnnotations()) {
+                        if (annoTag.getNumElems() < 1)
+                            continue;
+
+                        
+                        if (!annoTag.getType().matches(".*DS(Safe|Spec|Ban|Comment).*")) {
+                           continue; 
+                        }
+
+                        logger.debug("=========AnnotationTag=======");
+                        logger.debug("{}", annoTag);
+                        AnnotationElem elem = annoTag.getElemAt(0);
+                        logger.debug("TYPE:{}, ELEM-CLASS: {}, ELEM: {}", 
+                                annoTag.getType(), elem.getClass(), elem);
+                        
+                        if (elem instanceof AnnotationStringElem) {
+                            AnnotationStringElem strElem = (AnnotationStringElem)elem;
+                            logger.debug("TYPE: {}/ NAME:{}/VALUE: {}", 
+                                    annoTag.getType(), strElem.getName(), strElem.getValue());
+                            commentAnno = String.format("@DSComment(\"%s\")",strElem.getValue());
+                            logger.debug("comment Anno: {}", commentAnno);
+                        }
+
+                        if (elem instanceof AnnotationEnumElem) {
+                            AnnotationEnumElem enumElem = (AnnotationEnumElem)elem;
+                            logger.debug("TYPE:{}/ TYPENAME:{} /CONST_NAME{}: {}", enumElem.getName(), 
+                                    annoTag.getType(),
+                                    enumElem.getTypeName(),
+                                    enumElem.getConstantName());
+                            
+                            String[] tokens = annoTag.getType().split("/");
+
+                            String classificationType = tokens[tokens.length-1];
+                            classificationType = classificationType.replace(";", "");
+                            classificationAnno = String.format("@%s(DSCat.%s)", 
+                                    classificationType, enumElem.getConstantName());
+
+                            logger.debug("classification Type {}", classificationAnno);
+                        }
+                        logger.debug("==========================");
+                    }
+                }
+
+                String name = getMethodFullName(method);
+                //match list only when used with classification
+                if (!matchedMethodSignature(name) && classify)
+                    continue;
+
+                matchedSet.add(name);
+                String line = method.getSignature();
                 if (useModifier && method.getModifiers() != 0)
                     line = soot.Modifier.toString(method.getModifiers()) + ":" + line;
                 if (classify) {
@@ -57,12 +162,35 @@ public class ListApiMethods extends ApiUsageListing {
                    else if (SafeAndroidClassesAndMethods.v().isSafeMethod(method)) {
                        classification = "@DSSafe(DSCat.SAFE_LIST) - @DSComment(\"From safe class list\")";                                     
                    }
+                   
+                   if (classificationAnno != null) {
+                      if (commentAnno != null) {
+                          classificationAnno = classificationAnno + " - " + commentAnno;
+                      }
+                      classification  = "[E] " + classificationAnno;
+                   }
+
                    if (classification != null)
-                       line = classification + "-" +  line;         
+                       line = classification + " - " +  line;         
                 }
                 outStream.println(line);
             }
         }    
+        if (interestList.size() > 0) {
+            interestList.removeAll(matchedSet);
+            outStream.println("");
+            outStream.println("");
+            outStream.println("=================================================");
+            outStream.println("         Remained     ");
+            outStream.println("=================================================");
+            
+            List<String> remainedList = new ArrayList<String>(interestList);
+            Collections.sort(remainedList);          
+            
+            for (String api: remainedList) {
+                outStream.println("[U] " + api);
+            }
+        }
         outStream.close();
     }
     
@@ -75,6 +203,7 @@ public class ListApiMethods extends ApiUsageListing {
         options.addOption("o", "out",     true,  "output filename");
         options.addOption("c", "classify", false, "automatic classification");
         options.addOption("a", "apijar",  true,  "Optional API jar file");
+        options.addOption("l", "list",  true,  "list of api to list (class methodname)");
 
         if (args.length == 0){
             printHelp(options);
@@ -127,6 +256,10 @@ public class ListApiMethods extends ApiUsageListing {
         for (String jarName: libJars) {
             logger.warn("Loading API jar {} ", jarName);
             listing.addApiJar(jarName);
+        }
+        
+        if (commandLine.hasOption("l")) {
+            listing.setReadList(commandLine.getOptionValue("l"));
         }
         listing.activateSootScene();
         
