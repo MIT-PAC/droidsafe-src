f4f249930381dbda2be365e9c4e60c3d63f0bd0f
Michael Gordon <michael.ian.gordon@gmail.com>
2014-02-27 18:19:27 -0500

    more classifications

--- api/droidsafe/concrete/DroidSafeProjection.java
+++ api/droidsafe/concrete/DroidSafeProjection.java
@@ -7,18 +7,23 @@ import droidsafe.annotations.*;
 
 public class DroidSafeProjection implements Projection {
     
+    @DSSafe(DSCat.GUI)
     public DroidSafeProjection() {
     }
-    
+
+    @DSSafe(DSCat.GUI)
     public GeoPoint fromPixels(int x, int y) {
         return new GeoPoint();
     }
     
+    @DSSafe(DSCat.GUI)
+    @DSSource(DSSourceKind.LOCATION_INFORMATION)
     public float metersToEquatorPixels(float meters) { 
         addTaint(meters);
         return getTaintFloat();
     }
     
+    @DSSafe(DSCat.GUI)
     public Point toPixels(GeoPoint in, Point out) {
         Point point = new Point();
         out.addTaint(in.getTaint());
@@ -26,6 +31,9 @@ public class DroidSafeProjection implements Projection {
         in.addTaint(out.getTaint());
 
         point.addTaint(out.getTaint());
+        //transfer location taint from geopoint
+        point.set(in.getLatitudeE6() + out.x,
+                  in.getLongitudeE6() + out.y);
 
         return point;
     }
