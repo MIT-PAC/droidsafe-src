fc43ea879fcefafa17e8a2d40fa8c54085debd26
Michael Gordon <michael.ian.gordon@gmail.com>
2014-04-11 20:52:15 -0400

    reverting, I did not mean to check in the last version it was not a fix

--- main/java/droidsafe/analyses/infoflow/ObjectUtils.java
+++ main/java/droidsafe/analyses/infoflow/ObjectUtils.java
@@ -7,7 +7,7 @@ import soot.Scene;
 import soot.SootClass;
 import soot.SootField;
 import soot.SootMethod;
-import droidsafe.android.system.API;
+
 import droidsafe.main.Config;
 
 public class ObjectUtils {
@@ -26,34 +26,21 @@ public class ObjectUtils {
     Set<SootMethod> getTaints;
 
     private ObjectUtils() {
-        addTaints = new HashSet<SootMethod>();
-        getTaints = new HashSet<SootMethod>();
-        
-        //set taint field
-        SootClass objClass = Scene.v().getSootClass("java.lang.Object");
-        
-        taint = objClass.getFieldByName("taint");
+        SootClass klass = Scene.v().getSootClass("java.lang.Object");
+        if (Config.v().strict) {
+            assert klass != null;
+        }
+        taint = klass.getFieldByName("taint");
         if (Config.v().strict) {
             assert taint != null;
         }
-        
-        //addTaint and getTaint can appear in any class, so search all classes
-        for (SootClass klass : Scene.v().getClasses()) {
-            //search for addtaint and gettaint only in system classes
-            if (!API.v().isSystemClass(klass))
-                continue;
-            
-            if (Config.v().strict) {
-                assert klass != null;
-            }
-            
-            //could be multiple addTaint and getTaint* methods, so loop through all methods
-            for (SootMethod method : klass.getMethods()) {
-                if (method.getName().startsWith("addTaint")) {
-                    addTaints.add(method);
-                } else if (method.getName().startsWith("getTaint")) {
-                    getTaints.add(method);
-                }
+        addTaints = new HashSet<SootMethod>();
+        getTaints = new HashSet<SootMethod>();
+        for (SootMethod method : klass.getMethods()) {
+            if (method.getName().startsWith("addTaint")) {
+                addTaints.add(method);
+            } else if (method.getName().startsWith("getTaint")) {
+                getTaints.add(method);
             }
         }
     }
