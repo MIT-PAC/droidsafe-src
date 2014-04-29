67706630e83e30d65e2f92e85f3a8fff1d63f2f4
Limei Gilham <gilham@kestrel.edu>
2014-04-29 13:55:54 -0700

    Fixed a bug related to the syntax of default filters.

--- eclipse/droidsafe.eclipse.plugin.core/src/droidsafe/eclipse/plugin/core/filters/FilterOp.java
+++ eclipse/droidsafe.eclipse.plugin.core/src/droidsafe/eclipse/plugin/core/filters/FilterOp.java
@@ -5,7 +5,8 @@ import droidsafe.eclipse.plugin.core.util.DroidsafePluginUtilities;
 public enum FilterOp {
     EXCLUDE(0), INCLUDE(1), SHOW(2);
 
-    public static final String[] strings = DroidsafePluginUtilities.enumStrings(FilterOp.class);;
+    public static final String[] strings = DroidsafePluginUtilities.enumStrings(FilterOp.class);
+    public static final String[] labels = getLabels();
 
     private int value;
 
@@ -20,9 +21,25 @@ public enum FilterOp {
     public String toString() {
         switch (this) {
             case EXCLUDE: return "exclude";
+            case INCLUDE: return "include";
+            default: return "show";
+        }
+    }
+    
+    public String toLabel() {
+        switch (this) {
+            case EXCLUDE: return "exclude";
             case INCLUDE: return "include (if true for node and all parents)";
             default: return "include (if true for any children)";
         }
+    }
+    
+    public static String[] getLabels() {
+        FilterOp[] values = values();
+        String[] labels = new String[values.length];
+        for (int i = 0; i < values.length; i++)
+            labels[i] = values[i].toLabel();
+        return labels;
     }
     
     public static FilterOp parse(String str) throws FilterException {
