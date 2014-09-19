6137f84eccb58396e69ae6d3a8bccf6a4ba9dcc3
Nguyen Nguyen <nguyen@uwinsoftware.com>
2014-09-18 23:43:26 -0400

    Added appcompat/R.java

---
+++ api/android/support/v7/appcompat/R.java
@@ -0,0 +1,225 @@
+package android.support.v7.appcompat;
+
+public final class R {
+    public final class attr {
+        public static final int actionBarWidgetTheme = 0;
+        public static final int actionModeShareDrawable = 0;
+        public static final int searchViewSearchIcon = 0;
+        public static final int textColorSearchUrl = 0;
+
+        public static final int actionBarSize = 0;
+        public static final int actionBarStyle = 0;
+        public static final int actionBarTabTextStyle = 0;
+        public static final int actionDropDownStyle = 0;
+        public static final int actionModeStyle = 0;
+        public static final int actionOverflowButtonStyle = 0;
+        public static final int dropDownListViewStyle = 0;
+        public static final int homeAsUpIndicator = 0;
+        public static final int listPopupWindowStyle = 0;
+        public static final int popupMenuStyle = 0;
+        public static final int spinnerStyle = 0;
+
+
+
+    }
+
+    public final class anim {
+        public static final int abc_fade_in = 0;
+        public static final int abc_fade_out = 0;
+        public static final int abc_slide_in_bottom = 0;
+        public static final int abc_slide_in_top = 0;
+        public static final int abc_slide_out_bottom = 0;
+        public static final int abc_slide_out_top = 0;
+    }
+
+    public final class bool {
+        public static final int abc_action_bar_embed_tabs_pre_jb = 0;
+        public static final int abc_config_showMenuShortcutsWhenKeyboardPresent = 0;
+        public static final int abc_split_action_bar_is_narrow = 0;
+        public static final int abc_action_bar_expanded_action_views_exclusive = 0;
+    }
+
+    public final class dimen {
+        public static final int abc_action_bar_stacked_tab_max_width = 0;
+        public static final int abc_action_bar_stacked_max_height = 0;
+        public static final int abc_config_prefDialogWidth = 0;
+        public static final int abc_dropdownitem_icon_width = 0;
+        public static final int abc_dropdownitem_text_padding_left = 0;
+        public static final int abc_search_view_preferred_width = 0;
+    }
+
+    public final class id {
+        public static final int action_bar = 0;
+        public static final int action_bar_activity_content = 0;
+        public static final int action_bar_container = 0;
+        public static final int action_bar_root = 0;
+        public static final int action_bar_subtitle = 0;
+        public static final int action_bar_title = 0;
+        public static final int action_context_bar = 0;
+        public static final int action_menu_presenter = 0;
+        public static final int action_mode_close_button = 0;
+        public static final int activity_chooser_view_content = 0;
+        public static final int default_activity_button = 0;
+        public static final int edit_query = 0;
+        public static final int expand_activities_button = 0;
+        public static final int home = 0;
+        public static final int icon = 0;
+        public static final int image = 0;
+        public static final int progress_circular = 0;
+        public static final int progress_horizontal = 0;
+        public static final int search_button = 0;
+        public static final int search_close_btn = 0;
+        public static final int search_edit_frame = 0;
+        public static final int search_go_btn = 0;
+        public static final int search_mag_icon = 0;
+        public static final int search_plate = 0;
+        public static final int search_src_text = 0;
+        public static final int search_voice_btn = 0;
+        public static final int shortcut = 0;
+        public static final int split_action_bar = 0;
+        public static final int submit_area = 0;
+        public static final int title = 0;
+        public static final int top_action_bar = 0;
+        public static final int up = 0;
+        public static final int action_bar_overlay_layout = 0;
+        public static final int list_item = 0;
+    }
+
+    public final class integer {
+        public static final int abc_max_action_buttons = 0;
+    }
+
+    public final class layout {
+        public static final int abc_action_bar_decor = 0;
+        public static final int abc_action_bar_decor_overlay = 0;
+        public static final int abc_action_bar_tab = 0;
+        public static final int abc_action_bar_tabbar = 0;
+        public static final int abc_action_bar_title_item = 0;
+        public static final int abc_action_mode_close_item = 0;
+        public static final int abc_activity_chooser_view = 0;
+        public static final int abc_list_menu_item_checkbox = 0;
+        public static final int abc_list_menu_item_icon = 0;
+        public static final int abc_list_menu_item_layout = 0;
+        public static final int abc_list_menu_item_radio = 0;
+        public static final int abc_search_view = 0;
+        public static final int abc_action_bar_home = 0;
+        public static final int abc_action_bar_view_list_nav_layout = 0;
+        public static final int abc_action_menu_item_layout = 0;
+        public static final int abc_action_menu_layout = 0;
+        public static final int abc_activity_chooser_view_list_item = 0;
+        public static final int abc_expanded_menu_layout = 0;
+        public static final int abc_popup_menu_item_layout = 0;
+        public static final int abc_search_dropdown_item_icons_2line = 0;
+    }
+
+    public final class string { 
+        public static final int abc_activity_chooser_view_see_all = 0;
+        public static final int abc_action_bar_home_description = 0;
+        public static final int abc_action_bar_up_description = 0;
+        public static final int abc_activitychooserview_choose_application = 0;
+        public static final int abc_shareactionprovider_share_with = 0;
+        public static final int abc_shareactionprovider_share_with_application = 0;
+    }
+
+    public final class styleable {
+        public static final int ActionBar_background = 0;
+        public static final int ActionBar_customNavigationLayout = 0;
+        public static final int ActionBar_displayOptions = 0;
+        public static final int ActionBar_height = 0;
+        public static final int ActionBar_icon = 0;
+        public static final int ActionBar_itemPadding = 0;
+        public static final int ActionBarLayout_android_layout_gravity = 0;
+        public static final int ActionBar_logo = 0;
+        public static final int ActionBar_navigationMode = 0;
+        public static final int ActionBar_progressBarPadding = 0;
+        public static final int ActionBar_progressBarStyle = 0;
+        public static final int ActionBar_subtitle = 0;
+        public static final int ActionBar_subtitleTextStyle = 0;
+        public static final int ActionBar_title = 0;
+        public static final int ActionBar_titleTextStyle = 0;
+        public static final int ActionBarWindow_windowActionBar = 0;
+        public static final int ActionBarWindow_windowActionBarOverlay = 0;
+        public static final int ActionBarWindow_windowSplitActionBar = 0;
+        public static final int CompatTextView_textAllCaps = 0;
+        public static final int LinearLayoutICS_divider = 0;
+        public static final int LinearLayoutICS_dividerPadding = 0;
+        public static final int LinearLayoutICS_showDividers = 0;
+        public static final int MenuGroup_android_enabled = 0;
+        public static final int MenuGroup_android_id = 0;
+        public static final int MenuGroup_android_orderInCategory = 0;
+        public static final int MenuGroup_android_visible = 0;
+        public static final int MenuItem_actionLayout = 0;
+        public static final int MenuItem_actionProviderClass = 0;
+        public static final int MenuItem_actionViewClass = 0;
+        public static final int MenuItem_android_alphabeticShortcut = 0;
+        public static final int MenuItem_android_checkable = 0;
+        public static final int MenuItem_android_checked = 0;
+        public static final int MenuItem_android_enabled = 0;
+        public static final int MenuItem_android_icon = 0;
+        public static final int MenuItem_android_id = 0;
+        public static final int MenuItem_android_menuCategory = 0;
+        public static final int MenuItem_android_numericShortcut = 0;
+        public static final int MenuItem_android_onClick = 0;
+        public static final int MenuItem_android_orderInCategory = 0;
+        public static final int MenuItem_android_title = 0;
+        public static final int MenuItem_android_titleCondensed = 0;
+        public static final int MenuItem_android_visible = 0;
+        public static final int MenuItem_showAsAction = 0;
+        public static final int MenuView_android_itemBackground = 0;
+        public static final int SearchView_android_imeOptions = 0;
+        public static final int SearchView_android_inputType = 0;
+        public static final int SearchView_android_maxWidth = 0;
+        public static final int SearchView_iconifiedByDefault = 0;
+        public static final int SearchView_queryHint = 0;
+        public static final int Spinner_android_dropDownWidth = 0;
+        public static final int Spinner_android_gravity = 0;
+        public static final int Spinner_android_popupBackground = 0;
+        public static final int Spinner_prompt = 0;
+        public static final int Spinner_spinnerMode = 0;
+        public static final int Theme = 0;
+        public static final int View_android_focusable = 0;
+
+        public static final int ActionBar = 0;
+        public static final int ActionBar_backgroundSplit = 0;
+        public static final int ActionBar_backgroundStacked = 0;
+        public static final int ActionBar_homeLayout = 0;
+        public static final int ActionBar_indeterminateProgressStyle = 0;
+        public static final int ActionBarLayout = 0;
+        public static final int ActionBarWindow = 0;
+        public static final int ActionMenuItemView_android_minWidth = 0;
+        public static final int ActionMode = 0;
+        public static final int ActionMode_background = 0;
+        public static final int ActionMode_backgroundSplit = 0;
+        public static final int ActionMode_height = 0;
+        public static final int ActionMode_subtitleTextStyle = 0;
+        public static final int ActionMode_titleTextStyle = 0;
+        public static final int ActivityChooserView = 0;
+        public static final int ActivityChooserView_expandActivityOverflowButtonDrawable = 0;
+        public static final int ActivityChooserView_initialActivityCount = 0;
+        public static final int CompatTextView = 0;
+        public static final int LinearLayoutICS = 0;
+        public static final int MenuGroup = 0;
+        public static final int MenuGroup_android_checkableBehavior = 0;
+        public static final int MenuGroup_android_menuCategory = 0;
+        public static final int MenuItem = 0;
+        public static final int MenuView = 0;
+        public static final int MenuView_android_preserveIconSpacing = 0;
+        public static final int SearchView = 0;
+        public static final int Spinner = 0;
+        public static final int Spinner_android_dropDownHorizontalOffset = 0;
+        public static final int Spinner_android_dropDownVerticalOffset = 0;
+        public static final int Theme_panelMenuListTheme = 0;
+        public static final int View = 0;
+    }
+
+    public final class style {
+        public static final int Theme_AppCompat_CompactMenu = 0;
+        public static final int Theme_AppCompat_CompactMenu_Dialog = 0;
+    }
+}
+
+
+
+
+
+
