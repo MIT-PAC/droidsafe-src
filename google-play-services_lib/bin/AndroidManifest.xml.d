efc1d7c1845a4542aa9a85cc19d60aced4877d22
Michael Gordon <michael.ian.gordon@gmail.com>
2014-09-29 10:34:45 -0400

    google play services lib project

---
+++ google-play-services_lib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/mgordon/tools/android-sdk-macosx/extras/google/google_play_services/libproject/google-play-services_lib/bin/AndroidManifest.xml : \
+/Users/mgordon/tools/android-sdk-macosx/extras/google/google_play_services/libproject/google-play-services_lib/AndroidManifest.xml \
