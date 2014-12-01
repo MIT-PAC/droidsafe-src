bfd9756504a401fb00bcb3b253c60873606e04da
Michael Gordon <michael.ian.gordon@gmail.com>
2014-12-01 15:46:07 -0500

    track flows through exception detail messages

--- api/java/lang/Throwable.java
+++ api/java/lang/Throwable.java
@@ -101,7 +101,7 @@ public Throwable() {
     
 public Throwable(String detailMessage) {
         this();
-        this.detailMessage = new String();
+        this.detailMessage = detailMessage;  //new String();
     }
 
     /**
@@ -119,7 +119,7 @@ public Throwable(String detailMessage) {
     
 public Throwable(String detailMessage, Throwable throwable) {
         this();
-        this.detailMessage = new String();
+        this.detailMessage = detailMessage;//new String();
         cause = throwable;
     }
 
