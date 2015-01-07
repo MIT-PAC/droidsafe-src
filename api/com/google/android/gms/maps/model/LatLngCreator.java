e4c3efbfc22c90c611793dce8c0bf22d83441738
Nguyen Nguyen (Global InfoTek, Inc) <nguyen@uwinsoftware.com>
2015-01-07 12:04:07 -0500

    Added models for LatLngCreator

--- api/com/google/android/gms/maps/model/LatLngCreator.java
+++ api/com/google/android/gms/maps/model/LatLngCreator.java
@@ -1,11 +1,15 @@
 package com.google.android.gms.maps.model;
 
+import droidsafe.annotations.DSCat;
+import droidsafe.annotations.DSSafe;
 import android.os.Parcel;
 
 public class LatLngCreator {
 	public static final int CONTENT_DESCRIPTION = 0; // just a guess 
+    @DSSafe(DSCat.SAFE_OTHERS)
 	public LatLngCreator() {}
 	
+    @DSSafe(DSCat.SAFE_OTHERS)
 	public LatLng createFromParcel(Parcel parcel) {
 		LatLng latlng = new LatLng(0, 0);
 		latlng.latitude = parcel.readDouble();
@@ -13,6 +17,7 @@ public class LatLngCreator {
 		return latlng;
 	}
 	
+    @DSSafe(DSCat.SAFE_OTHERS)
 	public LatLng[] newArray(int size) {
 		return new LatLng[size];
 	}
